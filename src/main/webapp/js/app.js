559718564da8ef77a7521d383e6bf877d508d60e
Romain <Romain@PC_One>
2014-07-12 16:07:34 +0200

    Ajout d'une page de connexion (en cours)

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,47 +1,79 @@
 var app = angular.module("geektic", [ 'ngRoute', 'ui.gravatar',
 		'checklist-model' ]);
 
-app.config(function($routeProvider){
+/**
+ * CONFIGURATION
+ */
+app.config(function($routeProvider) {
 	$routeProvider.when('/', {
-        templateUrl: 'views/home.html',
-        controller: 'MainCtrl',
-        reloadOnSearch: false
-      }).when('/genre/:genre/interet/:interet', {
-    	  templateUrl: 'views/home.html',
-          controller: 'MainCtrl',
-          reloadOnSearch: false
-      }).when('/profil/:id', {
-        templateUrl: 'views/profil.html',
-        controller: 'ProfilCtrl'
-      })
+		templateUrl : 'views/home.html',
+		controller : 'MainCtrl',
+		reloadOnSearch : false
+	}).when('/genre/:genre/interet/:interet', {
+		templateUrl : 'views/home.html',
+		controller : 'MainCtrl',
+		reloadOnSearch : false
+	}).when('/profil/:id', {
+		templateUrl : 'views/profil.html',
+		controller : 'ProfilCtrl'
+	}).when('/login', {
+		templateUrl : 'views/login.html',
+		controller : 'AuthCtrl'
+	})
+
+	$routeProvider.otherwise({
+		redirectTo : '/login'
+	});
 
 })
 
-app.controller('MainCtrl', function($scope, $http, $routeParams, $location) {
+/**
+ * SERVICE FACTORY
+ */
+
+app.factory("AuthenticationService", function($location) {
+  return {
+    login: function(credentials) {
+      if (credentials.username !== "admin" || credentials.password !== "admin") {
+        alert("Incorrect Password or Username");
+      } else {
+        $location.path('/');
+      }
+    },
+    logout: function() {
+      $location.path('/login');
+    }
+  };
+});
+
+/**
+ * MAIN CONTROLLER
+ */
+app.controller('MainCtrl', function($scope, $http, $routeParams, $location, AuthenticationService) {
 
 	$http.get('/api/interet/').success(function(interets) {
 		$scope.interets = interets;
 	});
-	
+
 	$http.get('/api/geek/count').success(function(count) {
 		$scope.count = count;
-	}).error(function(count){
+	}).error(function(count) {
 		$scope.count = count;
 	});
 
 	// valeurs cochées par défaut
 	$scope.interetSel = [];
-	//$scope.interetSel.push(parseInt($routeParams.interet));
-	angular.forEach($routeParams.interet,function(interet){
-		$scope.interetSel.push(parseInt(interet));	
+	// $scope.interetSel.push(parseInt($routeParams.interet));
+	angular.forEach($routeParams.interet, function(interet) {
+		$scope.interetSel.push(parseInt(interet));
 	});
-	
+
 	$scope.interetParam = $routeParams.interet;
 
 	// fonction recherche
 	$scope.search = function() {
 		$location.search('genre', $scope.genre);
-		$location.search('interet', $scope.interetSel);		
+		$location.search('interet', $scope.interetSel);
 		$http.get(
 				'api/geek/genre/' + $scope.genre + '/interets/'
 						+ $scope.interetSel).then(function(response) {
@@ -49,25 +81,47 @@ app.controller('MainCtrl', function($scope, $http, $routeParams, $location) {
 		});
 	};
 
-	//$scope.genre = 'FEMME';
+	// $scope.genre = 'FEMME';
 	$scope.genre = $routeParams.genre;
 	$scope.chx_genre = function(genre) {
 		$scope.genre = genre;
 	};
-	
+
 	$scope.search();
-
-});
-
-app.controller('ProfilCtrl', function($scope, $http, $routeParams) {
-
-	$http.get('/api/geek/id/'+$routeParams.id).success(function(geek) {
-		$scope.geek = geek;
-	});
 	
-	  $scope.back = function() { 
-		    window.history.back();
+	$scope.logout = function() {
+	    AuthenticationService.logout();
 	  };
 
 });
 
+/**
+ * PROFIL CONTROLLER
+ */
+app.controller('ProfilCtrl', function($scope, $http, $routeParams) {
+
+	$http.get('/api/geek/id/' + $routeParams.id).success(function(geek) {
+		$scope.geek = geek;
+	});
+
+	$scope.back = function() {
+		window.history.back();
+	};
+
+});
+
+/**
+ * AUTH CONTROLLER
+ */
+app.controller('AuthCtrl', function($scope, $http, $routeParams, AuthenticationService) {
+
+	$scope.credentials = {
+		username : "",
+		password : ""
+	};
+
+	$scope.login = function() {
+	    AuthenticationService.login($scope.credentials);
+	  }
+
+});
